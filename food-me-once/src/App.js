dcf13a1b14249fb10ca5ef4bd3498b1332eeba23
Christopher Chasteen <cchas@utexas.edu>
2019-09-24 18:38:56 -0500

    Simple Beginning Changes

--- food-me-once/src/App.js
+++ food-me-once/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Food Me Once!
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Example Of a link if we want one!
         </a>
       </header>
     </div>
