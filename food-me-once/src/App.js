ea95f2848c93b0caed4a5726f529b1bbc8e10307
Christopher Chasteen <cchas@utexas.edu>
2019-09-24 18:56:55 -0500

    Example Changes

--- food-me-once/src/App.js
+++ food-me-once/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const exampleVar = 4;
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Example Of a link if we want one!
+          Example Of a link if we want one! {exampleVar}
         </a>
       </header>
     </div>
