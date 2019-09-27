6e500f33a9db210e7e7fa51a9c7281178cfe0e74
Christopher Chasteen <cchas@utexas.edu>
2019-09-27 17:03:27 -0500

    NavBar on all pages

--- src/components/App.js
+++ src/components/App.js
@@ -5,34 +5,48 @@ import Page2 from './Page2';
 import Page3 from './Page3';
 import Page4 from './Page4';
 import Page5 from './Page5';
+import {Navbar, Nav} from 'react-bootstrap'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Route 
-        exact path="/"
-        component={Page1}
-      />
-
-      <Route 
-        path="/Page2"
-        component={Page2}
-      />
-      <Route
-        path="/Page3"
-        component={Page3}
-      />
-      <Route
-        path="/Page4"
-        component={Page4}
-      />
-      <Route
-        path="/Page5"
-        component={Page5}
-      />
-    </BrowserRouter>
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">FoodMeOnce</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/page2">Districts</Nav.Link>
+            <Nav.Link href="/page2">Representatives</Nav.Link>
+            <Nav.Link href="/page2">Legislation</Nav.Link>
+            </Nav> 
+        </Navbar.Collapse>
+      </Navbar>
+      <BrowserRouter>
+        <Route 
+          exact path="/"
+          component={Page1}
+        />
+        <Route 
+          path="/Page2"
+          component={Page2}
+        />
+        <Route
+          path="/Page3"
+          component={Page3}
+        />
+        <Route
+          path="/Page4"
+          component={Page4}
+        />
+        <Route
+          path="/Page5"
+          component={Page5}
+        />
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/components/Page1.js
+++ src/components/Page1.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar, Nav} from 'react-bootstrap'
+
 import background from './cornField.jpg'
 
 function Page1() {
     return (
         <div>
-        <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">FoodMeOnce</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/page2">Districts</Nav.Link>
-            <Nav.Link href="/page3">Representatives</Nav.Link>
-            <Nav.Link href="/page4">Legislation</Nav.Link>
-            <Nav.Link href="/page5">About Us</Nav.Link>
-            </Nav> 
-        </Navbar.Collapse>
-        </Navbar>
         <img src={background} class="img-fluid" alt="Corn Field"/>
         </div>
     );
