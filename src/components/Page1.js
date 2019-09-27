2f3007b2b2243eeaa342479700a33067b5d9feef
Gyuwon Kim <wonkgo1@gmail.com>
2019-09-27 17:32:01 -0500

    splash page photo expands/shifts according to the screen width

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import background from './cornField.jpg'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import background from "./cornField.jpg";
 
 function Page1() {
-    return (
-        <div>
-        <img src={background} class="img-fluid" alt="Corn Field"/>
-        </div>
-    );
+  return (
+    <div>
+      <img
+        src={background}
+        class="img-fluid"
+        alt="Corn Field"
+        style={{ width: "100%" }}
+      />
+    </div>
+  );
 }
 
 export default Page1;
