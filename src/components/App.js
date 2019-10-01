c6910b95b8bd4c764a4cbc3c24a60ac4e2793175
Brian Dyck <you@example.com>
2019-10-01 12:43:41 -0500

    The icon now properly appears on the pages like it should.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Districts from "./Districts";
 import Representatives from "./Representatives";
