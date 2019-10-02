7aa56a5d46313c1d6a48b7f1987e5a6ede3c4ea7
Shubhendra Trivedi <shub.trivedi@utexas.edu>
2019-10-02 05:00:06 -0500

    Changed district instance to display card

--- src/components/InstancePages/DistrictInstance.js
+++ src/components/InstancePages/DistrictInstance.js
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactTable from 'react-table'
-import 'react-table/react-table.css'
+import District_img from '../../images/us.png';
+import "./css/District.css";
 
 class DistrictInstance extends React.Component{
 
@@ -8,47 +8,35 @@ class DistrictInstance extends React.Component{
         super(props)
     };
 
-//    render(){
-//    var district_data = [{
-//        name: this.props.location.state.name,
-//        avgIncome: this.props.location.state.avgIncome,
-//        avgEducation: this.props.location.state.avgEducation
-//        }]
-//
-//    console.log(this.props.location.state)
-//
-//    const columns = [{
-//    Header: 'District Name',
-//    accessor: 'name' // String-based value accessors!
-//  }, {
-//    Header: 'Average Income',
-//    accessor: 'avgIncome',
-//    //Cell: props => <span className='number'>{district_data.avgIncome}</span> // Custom cell components!
-//  }, {
-//    Header: 'Average Education', // Required because our accessor is not a string
-//    accessor: 'avgEducation',
-//    //accessor: d => d.friend.name // Custom value accessors!
-//  }]
-//    return(
-//        <ReactTable
-//    data={district_data}
-//    columns={columns}
-//  />
-//    );
-//    }
-    render() {
-        var district_data = this.props.location.state
-        //const data =[{"name":district_data.name},{"name":district_data.avgIncome}];
-        return (
-          <div className="districtdata">
-        <h1>{district_data.name}</h1>
-        <ul>
-          <li>Average Income: {district_data.avgIncome}</li>
-          <li>Average Education: {district_data.avgEducation}</li>
-          <li>Gender Ratio: {district_data.genderRatio}</li>
-          <li>Distance to food supply: {district_data.distToSupply}</li>
-        </ul>
-      </div>
+    render(){
+    var district_data = this.props.location.state
+
+    console.log(this.props.location.state)
+    return (
+    <div
+            className="district d-flex border border-secondary
+                justify-content-center flex-column align-items-center"
+        >
+            <img className="district-image" src={District_img} alt="us flag" />
+            <p className="district-name">{district_data.name}</p>
+            <ul>
+                <li className="district-desc">
+                    <span>Average Income</span>: ${district_data.avgIncome}
+                </li>
+                <li className="district-desc">
+                    <span>Average Education Level</span>: {district_data.avgEducation}
+                </li>
+                <li className="district-desc">
+                    <span>Average Age</span>: {district_data.avgAge}
+                </li>
+                <li className="district-desc">
+                    <span>Gender Ratio</span>: {district_data.genderRatio}
+                </li>
+                <li className="district-desc">
+                    <span>Distance to food supply</span>: {district_data.distToSupply}
+                </li>
+            </ul>
+        </div>
         );
       }
     }
