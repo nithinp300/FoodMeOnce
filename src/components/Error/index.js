2907a6da24fe838b0529f85b6c4258cbfa7c998d
Gyuwon Kim <wonkgo1@gmail.com>
2019-10-02 20:44:20 -0500

    Error page

---
+++ src/components/Error/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ErrorPage() {
+  return (
+    <div className="text-center">
+      <h1>404</h1>
+      <p>Sorry. We do not possess what you are trying to access.</p>
+      <p>Please go back and try again.</p>
+    </div>
+  );
+}
+
+export default ErrorPage;
