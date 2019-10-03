edc755e4a8ffc2e66d895320128e6cabb3330eda
Nithin Pingili <nithinp300@gmail.com>
2019-10-02 19:09:10 -0500

    added additional attributes for district instance

--- src/components/Districts/index.js
+++ src/components/Districts/index.js
@@ -16,7 +16,11 @@ class Districts extends Component {
         medianIncome: "71,486",
         avgAge: "37.9",
         genderRatio: "0.97",
-        representative: "Chip Roy"
+        representative: "Chip Roy",
+        senators: "John Cornyn, Ted Cruz",
+        peoplePerSquareMile: "136.7",
+        povertyRate: "10.3%",
+        numHouseholds: "315,100"
       },
       {
         name: "Texas 10th Congressional District",
@@ -24,7 +28,11 @@ class Districts extends Component {
         medianIncome: "75,517",
         avgAge: "35.9",
         genderRatio: "0.99",
-        representative: "Michael T. McCaul"
+        representative: "Michael T. McCaul",
+        senators: "John Cornyn, Ted Cruz",
+        peoplePerSquareMile: "166.9",
+        povertyRate: "7.9%",
+        numHouseholds: "290,104"
       },
       {
         name: "Texas 31st Congressional District",
@@ -32,7 +40,11 @@ class Districts extends Component {
         medianIncome: "70,346",
         avgAge: "35.2",
         genderRatio: "0.97",
-        representative: "Bill Flores"
+        representative: "Bill Flores",
+        senators: "John Cornyn, Ted Cruz",
+        peoplePerSquareMile: "396.3",
+        povertyRate: "8.9%",
+        numHouseholds: "288,768"
       },
     ]
   };
@@ -55,6 +67,10 @@ class Districts extends Component {
             avgAge={district.avgAge}
             genderRatio={district.genderRatio}
             representative={district.representative}
+            senators={district.senators}
+            peoplePerSquareMile={district.peoplePerSquareMile}
+            povertyRate={district.povertyRate}
+            numHouseholds={district.numHouseholds}
           />
         </Link>
       );
--- src/components/InstancePages/DistrictInstance.js
+++ src/components/InstancePages/DistrictInstance.js
@@ -1,12 +1,28 @@
 import React from "react";
 import District_img from '../../images/us.png';
 import "./css/DistrictInstance.css";
+import tx_21 from '../../images/tx_21.jpg';
+import tx_10 from '../../images/tx_10.jpg';
+import tx_31 from '../../images/tx_31.png';
 
 class DistrictInstance extends React.Component{
 
     constructor(props){
         super(props)
     };
+
+    getImage = (district_name) => {
+      if(district_name === "Texas 21st Congressional District"){
+           return tx_21;
+         }
+      if(district_name === "Texas 10th Congressional District"){
+          return tx_10;
+        }
+      if(district_name === "Texas 31st Congressional District"){
+          return tx_31;
+        }
+    return District_img;
+    }
 
     render(){
     var district_data = this.props.location.state
@@ -17,7 +33,7 @@ class DistrictInstance extends React.Component{
             className="district-instance d-flex border border-secondary
                 justify-content-center flex-column align-items-center"
         >
-            <img className="district-instance-image" src={District_img} alt="us flag" />
+            <img className="district-instance-image" src={this.getImage(district_data.name)} alt="us flag" />
             <p className="district-instance-name">{district_data.name}</p>
             <ul>
                 <li className="district-instance-desc">
@@ -35,6 +51,22 @@ class DistrictInstance extends React.Component{
                 <li className="district-instance-desc">
 
                     <span>Representative</span>: {district_data.representative}
+                </li>
+                <li className="district-instance-desc">
+
+                    <span>Senators</span>: {district_data.senators}
+                </li>
+                <li className="district-instance-desc">
+
+                    <span>Poverty Rate</span>: {district_data.povertyRate}
+                </li>
+                <li className="district-instance-desc">
+
+                    <span>Number of Households</span>: {district_data.numHouseholds}
+                </li>
+                <li className="district-instance-desc">
+
+                    <span>People per square mile</span>: {district_data.peoplePerSquareMile}
                 </li>
             </ul>
         </div>
