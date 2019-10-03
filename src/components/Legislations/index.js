ed8f1a2039dd5368e4a0ee99aff0c0b283056b3e
Gyuwon Kim <wonkgo1@gmail.com>
2019-10-03 00:37:41 -0500

    Legislation instances re-picked

--- src/components/Legislations/index.js
+++ src/components/Legislations/index.js
@@ -61,11 +61,11 @@ class Legislations extends Component {
         let sets = {};
         for (let i = 0; i < data.results[0].bills.length; ++i) {
           const name = data.results[0].bills[i].sponsor_name;
-          if (name === "K. Michael Conaway" || name === "Tim Ryan" || name === "Dave Loebsack") {
+          if (name === "K. Michael Conaway" || name === "Gerald E. Connolly" || name === "Dave Loebsack") {
             if (sets[name] == null) {
               sets[name] = name;
               legislations.push(data.results[0].bills[i]);
-            }
+            } 
           }
         }
         this.setState({ legislations });
