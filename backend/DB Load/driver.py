517cbacd3de8b758e39811808f28bc124757332c
Shubhendra Trivedi <shub.trivedi@utexas.edu>
2019-10-15 18:46:09 -0500

    cleaned up db load files and code, commenting and testing to go

--- backend/DB Load/driver.py
+++ backend/DB Load/driver.py
@@ -23,8 +23,6 @@ import xml.etree.ElementTree as et
 from sqlalchemy.orm import sessionmaker
 import psycopg2
 
-PARAMS = {'X-API-KEY': 'eqgLGZRNuOktoYkIpRdonPmtq4zIKokpsvT0EpN6'}
-
 def pgadminconnect():
     # conn = psycopg2.connect(host="foodmeonce.csja89mbwp6s.us-west-1.rds.amazonaws.com", user="postgres", password="ShameOnYou!")
     # print(conn)
@@ -57,16 +55,10 @@ def extract_json(data_json, api_number):
         df["cosponsors_by_party_D"] = df_2["D"]
         print(df[["cosponsors_by_party_D", "cosponsors_by_party_R"]])
         df = df.drop("cosponsors_by_party", axis=1)
-        # if df["cosponsors_by_party"].empty():
-        #     print(df["cosponsors_by_party"])
-        # df["cosponsors_by_party_R"] = df["cosponsors_by_party"]
-        # print(df['cosponsors_by_party'])
         return df
-    # return data_df
 
 
 def API_one_response(api_uri, api_number):
-    # url = 'https://api.propublica.org/congress/v1/116/house/members'
     param = {'X-API-KEY': API_creds.propublica()}
     res = requests.get(url = api_uri, headers = param)
     if res:
@@ -110,74 +102,10 @@ def API_one_response(api_uri, api_number):
         print('Connection Failed')
         print(res.status_code)
 
-def parse_element(element, parsed=None):
-    if parsed is None:
-        parsed = dict()
-    for key in element.keys():
-        if key not in parsed:
-            parsed[key] = element.attrib.get(key)
-        if element.text:
-            parsed[element.tag] = element.text
-        else:
-            raise ValueError("duplicate attribute {0} at element {1}".format(key, element.getroottree().getpath(element)))
-    for child in list(element):
-        parse_element(child, parsed)
-    return parsed
-
-
-def API_two_response():
-    url = 'https://api.propublica.org/congress/v1/bills/search.json?query=%22food+access%22'
-    param = {'X-API-KEY': 'eqgLGZRNuOktoYkIpRdonPmtq4zIKokpsvT0EpN6'}
-    res = requests.get(url = url, headers = param)
-    res = requests.get(url=url, headers=param)
-    if res:
-        try:
-            res.json()
-            print('\nSuccessful Connection . . . ')
-            print('\nContent in JSON format.')
-            # print(res.status_code)
-            # print(res.headers)
-            js = res.json()
-            print("\nStatus: " + js['status'] + '\n')
-            print(js['results'])
-            legislation_json = js['results']
-            members_df = json_normalize(legislation_json, 'bills')
-            # max_lengths = max_column_lengths(members_df)
-            return members_df
-
-        except Exception as e:
-            print('\nCritical failure!')
-            print(e)
-            print('\nRe-establishing connection . . . ')
-            # js = 'spam'
-            # print('Successful Connection . . . ')
-            print(res.status_code)
-            print(res.headers)
-            XMLdata = res['status']
-            root = et.XML(XMLdata)
-            print(root.getchildren())
-            structure_data = [parse_element(child) for child in root.getchildren()]
-            return pd.DataFrame(structure_data)
-    else:
-        print('Connection Failed')
-        print(res.status_code)
-
-def create_base_tables():
-    c.execute('CREATE TABLE IF NOT EXISTS )')
-
 def max_column_lengths(df):
     measurer = np.vectorize(len)
     result = measurer(df.values.astype(str)).max(axis=0)
-    # print(result)
     return result
-
-def connect_test():
-    conn_string = "host="+ creds.PGHOST +" port="+ "5432" +" dbname="+ creds.PGDATABASE +" user=" + creds.PGUSER \
-+" password="+ creds.PGPASSWORD
-    conn = psycopg2.connect(conn_string)
-    print(conn)
-    cursor = conn.cursor()
-    return conn
 
 def load_data(schema, table, conn, data, engine):
     print("Loading data into " + str(schema) + '.' + str(table))
@@ -188,10 +116,9 @@ def load_data(schema, table, conn, data, engine):
     except Exception as e:
         print(e)
     conn.close()
-    # loaded_data = pd.read_sql(sql_command, conn)
-    # print(engine.table_names())
 
-def main():
+
+if __name__ == "__main__":
     apis = ['https://api.propublica.org/congress/v1/116/house/members', 'https://api.propublica.org/congress/v1/bills/search.json?query=%22food+access%22','https://api.propublica.org/congress/v1/bills/search.json?query=%22food+security%22','https://api.propublica.org/congress/v1/bills/search.json?query=%22food+desert%22']
     table_names = ['congress_members', 'legislations']
     # api_counter = 0
@@ -203,19 +130,3 @@ def main():
         #     load_data('staging', table_names[0], db_objects[0], df, db_objects[1])
         # else:
         #     load_data('staging', table_names[1], db_objects[0], df, db_objects[1])
-    # legislation_df = API_two_response()
-    # print(members_df)
-    # df = API_two_response()
-    # print(df)
-    # create_base_tables()
-    # db_objects = pgadminconnect()
-    # new_session = db_session(db_objects[1])
-    # members_df = API_one_response()
-    # members_df.to_sql('staging.congress_members', db_objects[0])
-    # print(db_objects[1].table_names())
-    # print(db_objects)
-    # conn = connect_test()
-    # members_df = members_df.drop(['index'], axis=1)
-    # load_data('staging', 'congress_members', db_objects[0], members_df, db_objects[1])
-    # print(new_session)
-main()
