ed4b1e574507e93a66bbf35c9913a073513e7b45
Nithin Pingili <nithinp300@gmail.com>
2019-10-18 19:23:35 -0500

    created unittests for database loader

--- backend/DB_Load/testDBloader.py
+++ backend/DB_Load/testDBloader.py
@@ -1,5 +1,6 @@
 from unittest import main,TestCase
 from timeit import timeit
+from backend_Census_getCensusApiForDistrict import getStateNumbers
 # from driver import pgadminconnect
 
 
@@ -8,6 +9,16 @@ class TestDBloader(TestCase):
     def test_connect(self):
         i = 1
         assert i == 1
+
+    # check if we are loading in data for 50 states
+    def test_states(self):
+        states_dict = getStateNumbers()
+        num_states = len(states_dict)
+        assert num_states == 50
+
+    # check if we are loading in data for 435 districts
+    def test_districts(self):
+        pass
 
 if __name__ == "__main__":  # pragma: no cover
     main()
