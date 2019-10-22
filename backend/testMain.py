161b20b24eef074b8eb9f24a97254221d1245d2b
Nithin Pingili <nithinp300@gmail.com>
2019-10-22 15:25:38 -0500

    added assertEquals

--- backend/testMain.py
+++ backend/testMain.py
@@ -6,21 +6,21 @@ class TestMain(TestCase):
     def test_district(self):
         district_data = district("0")
         district_id = "35"
-        assert(district_data.congressional_district, district_id)
+        assertEquals(district_data.congressional_district, district_id)
 
     # test representative instance data
     def test_representative(self):
         rep_data = representative("0")
         rep_first_name = "Dan"
         rep_last_name = "Crenshaw"
-        assert(rep_data.first_name, rep_first_name)
-        assert(rep_data.last_name, rep_last_name)
+        assertEquals(rep_data.first_name, rep_first_name)
+        assertEquals(rep_data.last_name, rep_last_name)
 
     # test legislation instance data
     def test_legislation(self):
         legislation_data = legislation("0")
         legislation_title = "Agriculture Improvement Act of 2018"
-        assert(legislation_data.short_title, legislation_title)
+        assertEquals(legislation_data.short_title, legislation_title)
 
 if __name__ == "__main__":
     main()
