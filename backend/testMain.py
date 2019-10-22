ae6b45bad2d175758f205fabc9f7975db85e445d
Nithin Pingili <nithinp300@gmail.com>
2019-10-22 15:20:51 -0500

    main tests

--- backend/testMain.py
+++ backend/testMain.py
@@ -4,23 +4,23 @@ from main import district, representative, legislation
 class TestMain(TestCase):
     # test district instance data
     def test_district(self):
-        district_data = district(1)
-        district_id = "01"
-        assert(district_data.district, district_id)
+        district_data = district("0")
+        district_id = "35"
+        assert(district_data.congressional_district, district_id)
 
     # test representative instance data
     def test_representative(self):
-        rep_data = representative(0)
-        rep_first_name = "Ralph"
-        rep_last_name = "Abraham"
+        rep_data = representative("0")
+        rep_first_name = "Dan"
+        rep_last_name = "Crenshaw"
         assert(rep_data.first_name, rep_first_name)
         assert(rep_data.last_name, rep_last_name)
 
     # test legislation instance data
     def test_legislation(self):
-        legislation_data = legislation(0)
-        legislation_title = "Healthy Food Access for All Americans Act"
-        assert(legislation_data.title, legislation_title)
+        legislation_data = legislation("0")
+        legislation_title = "Agriculture Improvement Act of 2018"
+        assert(legislation_data.short_title, legislation_title)
 
 if __name__ == "__main__":
     main()
