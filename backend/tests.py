2bc4e54404fba0a1dbed446b36ac0f9215fcbbd7
Shubhendra Trivedi <shub.trivedi@utexas.edu>
2019-10-22 20:39:52 -0500

    path updated

--- backend/tests.py
+++ backend/tests.py
@@ -3,9 +3,7 @@ import sqlalchemy
 import pandas
 from timeit import timeit
 import sys
-sys.path.append('DB_Load')
-print('hello!!!!!!!!!!!!!!!!!')
-print(sys.path)
+sys.path = sys.path.append('DB_Load')
 from backend_Census_getCensusApiForDistrict import getStateNumbers, getData
 from backend_Scrapper import getJsonFromUrl
 from driver import pgadminconnect, API_response
