dc59c98d2b56c0cd01b5048e2acd6479a238b6ef
Christopher Chasteen <cchas@utexas.edu>
2019-11-04 20:08:45 -0600

    searching?

--- frontend/src/components/Representatives/index.js
+++ frontend/src/components/Representatives/index.js
@@ -46,7 +46,9 @@ class Representatives extends Component {
     metaData: {
       currentPage: 1,
       lastPage: 1
-    }
+    },
+    searchQueries: [],
+    searchField: ""
   };
 
   getType = type_flag => {
@@ -62,22 +64,57 @@ class Representatives extends Component {
     }));
   };
 
+  search = (representative) => {
+    // console.log(this.state.searchQueries[0])
+    //Add Fetch to api to set representatives and metadata here
+    console.log()
+    if(this.state.searchQueries.length > 0){
+      var found = true
+      this.state.searchQueries.forEach((elem)=>{
+        var query = elem.toLowerCase()
+        if(!(representative.first_name.toLowerCase().includes(query) ||
+            representative.last_name.toLowerCase().includes(query) ||
+            this.getAge(representative.date_of_birth).toString().includes(query) ||
+            representative.seniority.toString().includes(query) ||
+            this.getParty(representative.party).toLowerCase().includes(query) ||
+            representative.state.toLowerCase().includes(query) ||
+            representative.district.toLowerCase().includes(query)
+        
+        ))
+          found = false
+        // if( ! (
+        //   representative.first_name.toLowerCase().includes(query) ||
+        //   representative.last_name.toLowerCase().includes(query))
+        
+        
+        // ){
+        //   return false
+        // }
+      })
+     }
+    else return true
+    return found
+  }
 
   componentDidMount() {
     const querystring = this.props.location.search;
     const pathname = this.props.location.pathname;
-    const url = `https://api.foodmeonce.me${pathname}${querystring}`;
+    // console.log(querystring)
+    const url = `https://api.foodmeonce.me/Representatives`;
     fetch(url)
       .then(response => response.json())
       .then(data => {
         let representatives = data["data"];
+        console.log(representatives)
         let metaData = data["metaData"];
         this.setState({ representatives, metaData });
       })
       .catch(console.log);
   }
   render() {
-    const representativesRendered = this.state.representatives.map(
+
+    var tempRender = this.state.representatives.filter( (representative) => this.search(representative) )
+    var representativesRendered = tempRender.map(
       (representative, i) => {
         var rep_image = "";
         if (representative.first_name != null) {
@@ -105,11 +142,43 @@ class Representatives extends Component {
         );
       }
     );
+    //Splice total list to get only the ones for the page we are on
+    //representativesRendered = representativesRendered.splice(0,8)
     return (
       <div className="representatives-model">
+        
         <div className="sorting-container">
+        
           <div className="d-flex flex-row justify-content-between">
             <h3 className="ml-1">Representatives</h3>
+            <input class="form-control" type="text" 
+            onKeyPress={event =>{
+              if(event.key === 'Enter') {
+                // console.log(this.state.searchField)
+
+                var value = this.state.searchField
+                if(value=== "")
+                  this.setState(prevState => ({
+                  searchQueries: []
+                }));
+                else{
+                  this.setState(prevState => ({
+                  searchQueries: value.split(" ")
+                }));
+              }
+                //console.log(this.state.searchQueries)
+              }
+            }}
+
+            onChange={event => {
+                // console.log(event.target.value)
+                this.setState({
+                  searchField: event.target.value
+                })
+                // console.log(this.state)
+            }}
+            placeholder="Search" 
+            style={{marginLeft:"15%"}}aria-label="Search"/>
             <button
               className="ml-2 btn btn-secondary"
               onClick={this.handleCollapse}
