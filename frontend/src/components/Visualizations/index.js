19af562be39c412f27cb2beac68a4169fe5e039d
Shubhendra Trivedi <shub.trivedi@utexas.edu>
2019-11-26 17:15:09 -0600

    added in collapse buttons

--- frontend/src/components/Visualizations/index.js
+++ frontend/src/components/Visualizations/index.js
@@ -4,18 +4,32 @@ import LegislationHistogram from "./LegislationHistogram";
 import * as d3 from "d3";
 import RepresentativesBubbleChart from "./RepresentativesBubbleChart";
 import styling from "./css/styling.css";
+import DistrictSortFilter from "../Districts/DistrictSortFilter";
 
 class Visualizations extends React.Component {
   state = {
     data: [12, 5, 6, 6, 9, 10],
     width: 400,
     height: 300,
-    scale: 20
+    scale: 20,
+    collapse_1: true,
+      collapse_2: true,
+      collapse_3: true
   };
 
-    handleCollapse = () => {
+    handleCollapse_1 = () => {
     this.setState(prevState => ({
-      collapse: !prevState.collapse
+        collapse_1: !prevState.collapse_1
+    }));
+  };
+    handleCollapse_2 = () => {
+    this.setState(prevState => ({
+        collapse_2: !prevState.collapse_2
+    }));
+  };
+    handleCollapse_3 = () => {
+    this.setState(prevState => ({
+        collapse_3: !prevState.collapse_3
     }));
   };
 
@@ -25,18 +39,45 @@ class Visualizations extends React.Component {
     return (
       <div>
         <h1 className="text-center mt-2">Congressional District SNAP/Poverty Ratio Heat Map</h1>
-
-              <CongressionalDistricts/>
+          <div className="sorting-container">
+            <button
+              className="ml-2 btn btn-secondary"
+              onClick={this.handleCollapse_1}
+            >
+              {this.state.collapse_1 ? "-" : "+"}
+            </button>
+              {this.state.collapse_1 && (
+            <CongressionalDistricts/>)}
+          </div>
 
         <h1 className="text-center mt-2">Legislation by Representative Bubble Chart</h1>
-        <RepresentativesBubbleChart />
+                  <div className="sorting-container">
+            <button
+              className="ml-2 btn btn-secondary"
+              onClick={this.handleCollapse_2}
+            >
+              {this.state.collapse_2 ? "-" : "+"}
+            </button>
+              {this.state.collapse_2 && (
+            <RepresentativesBubbleChart/>)}
+          </div>
         <h1 className="text-center mt-2">Legislations by State Histogram</h1>
-        <LegislationHistogram
+                    <div className="sorting-container">
+            <button
+              className="ml-2 btn btn-secondary"
+              onClick={this.handleCollapse_3}
+            >
+              {this.state.collapse_3 ? "-" : "+"}
+            </button>
+              {this.state.collapse_3 && (
+                    <LegislationHistogram
           data={this.state.data}
           width={this.state.width}
           height={this.state.height}
           scale={this.state.scale}
-        />
+        />)}
+          </div>
+
       </div>
     );
   }
