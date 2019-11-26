76b936e8d0ff286cb24c7581fc1774d5ea99cf7d
Shubhendra Trivedi <shub.trivedi@utexas.edu>
2019-11-26 16:40:26 -0600

    styling

--- frontend/src/components/Visualizations/index.js
+++ frontend/src/components/Visualizations/index.js
@@ -3,6 +3,7 @@ import CongressionalDistricts from "./CongressionalDistricts";
 import LegislationHistogram from "./LegislationHistogram";
 import * as d3 from "d3";
 import RepresentativesBubbleChart from "./RepresentativesBubbleChart";
+import styling from "./css/styling.css";
 
 class Visualizations extends React.Component {
   state = {
@@ -17,12 +18,11 @@ class Visualizations extends React.Component {
   render() {
     return (
       <div>
-        <h1 className="text-center mt-2">Snap to Poverty Map of Congressional Districts</h1>
-        
+        <h1 className="text-center mt-2">Congressional District SNAP/Poverty Ratio Heat Map</h1>
         <CongressionalDistricts />
-        <h1 className="text-center mt-2">Legislation Per Congressmen</h1>
+        <h1 className="text-center mt-2">Legislation by Representative Bubble Chart</h1>
         <RepresentativesBubbleChart />
-        <h1 className="text-center mt-2">Legislations raised by state</h1>
+        <h1 className="text-center mt-2">Legislations by State Histogram</h1>
         <LegislationHistogram
           data={this.state.data}
           width={this.state.width}
